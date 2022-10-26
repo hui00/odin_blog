eb4cda273fb872adb70986fa99ed2ba5257b696e
hui00 <kevinbrinkschulte@hotmail.de>
2022-10-26 18:31:49 +0200

    production change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :articles
-  resoucres :home, only: [:index]
+  resources :home, only: [:index]
 
   # Defines the root path route ("/")
   root "home#index"
